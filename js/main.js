f9239070bce316373b7f87a2d3b3ba7949b0112f
Tom Woodward <twwoodward@me.com>
2020-08-10 11:29:25 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const sourceUrl = document.querySelector('#source-url');
+const embedCode = document.querySelector('#embed-code');
+
+sourceUrl.addEventListener('change', makeNew)
+
+function makeNew(){
+	console.log('ribbitt ribit')
+}
+
+
+console.log(sourceUrl)
+
+console.log(embedCode)
